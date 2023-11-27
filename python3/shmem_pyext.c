79da920f995bab5dac9586d376a275a76d1ae562
zhuichao001 <zhuichao001@163.com>
2023-11-27 15:22:02 +0800

    mod...

---
+++ python3/shmem_pyext.c
@@ -0,0 +1,48 @@
+#include "Python.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define FILE_MODE 0X766
+
+int shmem_open(const char *path){
+    int flags = O_RDWR | O_CREAT;
+    int fd = shm_open(path, flags, FILE_MODE); 
+    return fd;
+}
+
+static PyObject *py_shm_open(PyObject *self, PyObject *args) {
+    char *path;
+    int map_size;
+    if (!PyArg_ParseTuple(args, "s", &path)) {
+        return NULL;
+    }
+    int fd = shmem_open(path);
+    return Py_BuildValue("i", fd);
+}
+
+/* Module method table */
+static PyMethodDef methods[] = {
+    {"shm_open",  py_shm_open, METH_VARARGS, "shmem open"},
+    { NULL, NULL, 0, NULL}
+};
+
+/* Module structure */
+static struct PyModuleDef samplemodule = {
+    PyModuleDef_HEAD_INIT,
+    "shm_open",           /* name of module */
+    "A sample module",  /* Doc string (may be NULL) */
+    -1,                 /* Size of per-interpreter state or -1 */
+    methods       /* Method table */
+};
+
+/* Module initialization function */
+PyMODINIT_FUNC
+PyInit_shmem(void) {
+    return PyModule_Create(&samplemodule);
+}
